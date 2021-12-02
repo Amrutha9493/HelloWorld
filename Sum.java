1c9d952f89da1a65c52ac56cafdd5f7476461dae
Amrutha9493 <91303139+Amrutha9493@users.noreply.github.com>
2021-12-02 10:52:50 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author STUDENT
+ */
+public class Sum {
+     public static void main(String args[]){
+        int a=5,b=4,sum;
+        sum=a+b;
+        System.out.println("SUM:"+sum);
+    }
+}
